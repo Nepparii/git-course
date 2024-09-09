c183be4078fcd16268644f7b1a81c933164b6191
Nea <nea.tomminen@gmail.com>
2024-09-09 15:56:28 +0300

    Merge issues resolved

--- hello_world.py
+++ hello_world.py
@@ -1,11 +1,2 @@
-<<<<<<< HEAD
-
-def main():
-    print("Hello world!")
-
-if __name__ == "__main__":
-    main()
-=======
 print("Hello world!")
 print("Hello once again")
->>>>>>> feature/add-logging
